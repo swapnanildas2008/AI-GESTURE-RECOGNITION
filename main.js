11bed17b8e01c104cc7406045e31dcbfce6fb654
swapnanildas2008 <73822779+swapnanildas2008@users.noreply.github.com>
2021-05-25 13:40:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+nosex = 0;
+nosey = 0;
+leftwristy = 0;
+rightwristy = 0;
+diffrence = 0;
+function setup(){
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    video.position();
+    classifier = ml5.poseNet(video,modelloaded);
+    classifier.on('pose',gotresults);
+}
+
+function draw(){
+    background('#969a97');
+    document.getElementById("square_side").innerHTML = "width and height of square is"+diffrence+" px";
+    fill('#f90093');
+    stroke('#f90093');
+    square(nosex,nosey,diffrence);
+}
+
+function modelloaded(){
+    console.log("model loaded successfully");
+}
+
+function gotresults(r){
+    if(r.length > 0){
+        console.log(r);
+        nosex = r[0].pose.nose.x;
+        nosey = r[0].pose.nose.y;
+        leftwristy = r[0].pose.leftWrist.y;
+        rightwristy = r[0].pose.rightWrist.y;
+        diffrence = Math.floor(leftwristy - rightwristy);
+    }
+}
